8514ae5903c1e7970e1ef629439c52f2c51b5f0d
Near <n@orapp.org>
2025-04-30 01:00:31 +0900

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,94 +2,35 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
+    <main className="h-screen pt-20">
+      
+      {/* 自己紹介*/}
+      <div>
+          <div  className="flex flex-col items-center content-center justify-center pb-10">
+            <Image src="/ico.jpg" alt="Logo" unoptimized width={200} height={200} className="h-50 w-50 mr-0 rounded-full border-4 border-gray-600" /> 
+            <p className="text-gray-800 text-center text-2xl pt-10 nyak-color"><ruby>社甘<rp>(</rp><rt>しゃっかん</rt><rp>)</rp></ruby> <ruby>疒<rp>(</rp><rt>にゃく</rt><rp>)</rp></ruby></p>
+            <p className="text-gray-800 text-center text-1xl pt-2 nyak-color">映像制作とプログラミング、割となんでもします。</p>
+            <div className="flex pt-5">
+              <a href="https://x.com/nyakukonyaku"><Image src="/x.svg" alt="Logo" unoptimized width={200} height={200} className="h-6 w-6 mr-4" /> </a>
+              <a href="https://www.youtube.com/channel/UCAoYdKiquahmtahltyC2a9A"><Image src="/yt.svg" alt="Logo" unoptimized width={200} height={200} className="h-6 w-6 mr-4" /> </a>
+              <a href="https://www.amazon.co.jp/hz/wishlist/ls/3ID20HKG47QQT?ref_=wl_share"><Image src="/amazon.svg" alt="Logo" unoptimized width={200} height={200} className="h-6 w-6 mr-4 " /> </a>
+            </div>
+          </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      {/* 実績 */}
+      <div>
+        <p className="text-center text-2xl pb-5">制作実績</p>
+        <div className="text-center">
+          <div className="flex flex-wrap justify-center">
+              <iframe className="w-64 h-36 rounded-xl m-5" width="480" height="270" src="https://www.youtube.com/embed/J6j74qOO9LY?si=YKVBhauD4UPsgv-F&autoplay=1&mute=1" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
+              <iframe className="w-64 h-36 rounded-xl m-5" width="480" height="270" src="https://www.youtube.com/embed/1YKATtmczPs?si=YKVBhauD4UPsgv-F&autoplay=1&mute=1" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
+              <a href="https://www.foriio.com/works/1499693"><Image src="/charact_webcomponent.jpg" width={200} height={200}  alt="Charact Webcomponent 制作" className="w-64 h-36 rounded-xl m-5"/> </a>
+              <iframe className="w-64 h-36 rounded-xl m-5" width="480" height="270" src="https://www.youtube.com/embed/RlAae_CrhII?si=YKVBhauD4UPsgv-F&autoplay=1&mute=1" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>  
+              <iframe className="w-64 h-36 rounded-xl m-5" width="480" height="270" src="https://www.youtube.com/embed/n6smirq8gpk?si=YKVBhauD4UPsgv-F&autoplay=1&mute=1" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
+          </div>  
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      
     </main>
   );
 }
